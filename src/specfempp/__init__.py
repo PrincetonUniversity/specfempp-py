a0aba1fd1fc530245356d253222ee90b30db6180
Lucas Sawade <lsawade@princeton.edu>
2025-03-10 15:25:38 -0400

    Fixed the commented part

--- src/specfempp/__init__.py
+++ src/specfempp/__init__.py
@@ -1,11 +1,11 @@
 import atexit
 
-# from specfempp_core import _initialize, _finalize
+from specfempp_core import _initialize, _finalize
 
 from .config import Config
 from .execute import execute
 
-# if _initialize([]):
+if _initialize([]):
     # atexit.register(_finalize)
 
 
