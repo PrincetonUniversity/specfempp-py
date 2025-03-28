01d604ed98f197f3a83c1298c867828301eb238a
Lucas Sawade <lsawade@princeton.edu>
2025-03-28 13:00:00 -0400

    Try error block working as expected

--- src/specfempp/execute.py
+++ src/specfempp/execute.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import sys
 import yaml
 from typing import TYPE_CHECKING
 
@@ -19,34 +20,12 @@ def execute(config: Config):
     # See specfem::periodic_tasks::check_signals
     
     try:
-        
         _execute(
             yaml.dump(config._runtime_config, sort_keys=False),
             yaml.dump(config._default_config, sort_keys=False)
             )
-
-    except RuntimeError as e:
-        print(e)
-        print(e)
-        print(e)
-        if "SIGNAL" in str(e):
-            code = int(str(e).rsplit(" ", 1)[1])
-            
-            if code == "SIGINT" or code == "2":
-                print("Simulation was interrupted.")
-                raise KeyboardInterrupt
-            elif code == "SIGKILL" or code == "9":
-                print("Simulation was killed.")
-                raise KeyboardInterrupt
-            elif code == "SIGTERM" or code == "15":
-                print("Simulation was terminated.")
-                raise KeyboardInterrupt
-            elif code == "SIGSEGV" or code == "11":
-                print("Simulation was terminated due to a segmentation fault.")
-                raise KeyboardInterrupt
-            elif code == "SIGABRT" or code == "6":
-                print("Simulation was terminated due to an abort signal.")
-                raise KeyboardInterrupt
+    except Exception as e:
+        if "Signal" in str(e):
+            print("RuntimeError: Simulation interrupted by user", file=sys.stderr)
         else:
             raise e
-        
